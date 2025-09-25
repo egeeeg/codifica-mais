740056f41597b4588c1e00de0ee0815afca41c02
Dhavy <dhavy.rtalc@gmail.com>
2025-09-25 19:17:59 -0300

    Segundo Exercício correto

--- PHP-ARRAYS/dr-cod-php-a1.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$conta = readline("Qual o valor da conta? ");
-$gorjeta = readline("Qual a porcentagem da gorjeta? ");
-echo "O valor total com gorjeta foi: " . $conta * $gorjeta / 100 + $conta;

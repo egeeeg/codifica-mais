2c2f8ffd0978685043ca0b905b5329ce9d1ccefe
egeeeg <dhavy.rtalc@gmail.com>
2025-09-29 22:45:16 -0300

    Oitavo Exercício

---
+++ PHP-ARRAYS/dr-cod-php-a8.php
@@ -0,0 +1,23 @@
+<?php
+
+function aplicarDesconto($valorCompra, $percentualDesconto) {
+    $desconto = $valorCompra * ($percentualDesconto / 100);
+    return $valorCompra - $desconto;
+}
+
+function calcularDescontoProgressivo($valorCompra) {
+    if ($valorCompra < 100) {
+        return $valorCompra;
+    } elseif ($valorCompra <= 500) {
+        return aplicarDesconto($valorCompra, 10);
+    } else {
+        return aplicarDesconto($valorCompra, 20);
+    }
+}
+
+echo "Digite o valor da compra: R$ ";
+$valorCompra = floatval(trim(fgets(STDIN)));
+
+$valorFinal = calcularDescontoProgressivo($valorCompra);
+
+echo "Valor final após desconto progressivo: R$ " . number_format($valorFinal, 2, ',', '.') . PHP_EOL;

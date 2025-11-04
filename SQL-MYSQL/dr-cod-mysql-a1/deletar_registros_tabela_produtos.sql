c77c9563183434831e62ec9150312e04e152579f
Dhavy Rangel <dhavy.rtalc@gmail.com>
2025-11-04 19:02:28 -0300

    Segundo Exercício

--- SQL-MYSQL/dr-cod-mysql-a1/deletar_registros_tabela_produtos.sql
+++
@@ -1,7 +0,0 @@
-USE gestao_produtos;
-
-UPDATE produtos 
-SET 
-    deletado_em = CURRENT_TIMESTAMP
-WHERE 
-    id = 8;
--- SQL-MYSQL/dr-cod-mysql-a1/inserir_registros_tabela_fornecedores.sql
+++
@@ -1,8 +0,0 @@
-USE gestao_produtos;
-
-INSERT INTO 
-    fornecedores (razao_social, cnpj) 
-VALUES 
-    ('Tech Importados', '11223344000155'),
-    ('Distribuidora S.A.', '22334455000166'),
-    ('Varejista Comércio', '33445566000177');

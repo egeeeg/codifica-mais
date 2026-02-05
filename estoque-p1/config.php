69cf9e5c60400ebcdf5c5aa68553f41250532a8b
Dhavy <dhavy.rtalc@gmail.com>
2026-02-05 19:33:03 -0300

    Estoque de verdade

--- estoque-p1/config.php
+++ estoque-p1/config.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $dbname = 'estoque_p1';
 $user = 'root';
-$pass = 'R@ngel#22';
+$pass = '';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
